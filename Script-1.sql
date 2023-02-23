23af175ec1eb05f73e29ff4bbd6ff7f58d95f07d
YangHyeonJun <124110658+yang960920@users.noreply.github.com>
2023-02-23 17:40:02 +0900

    Add files via upload

---
+++ Script-1.sql
@@ -0,0 +1,69 @@
+ -- 테이블명 + 항목명 + 항목에 들어가는 데이터타입
+
+CREATE TABLE  member2  ( 
+	id varchar2(100),
+	pw varchar2(100),
+	name varchar2(100),
+	tel varchar2(100)
+	
+	)
+	
+	CREATE TABLE BBS2 (
+	id varchar2(100),
+	title varchar2(100),
+	content varchar2(100),
+	writer varchar2(100)
+	
+	
+	)
+	
+	
+	CREATE TABLE "HR"."PRODUCT2" 
+   (	"ID" VARCHAR2(100), 
+	"NAME" VARCHAR2(100), 
+	"CONTENT" VARCHAR2(100), 
+	"PRICE" NUMBER(38,0)
+   )
+	
+	
+   
+   CREATE TABLE "HR"."member2" 
+   (	"ID" VARCHAR2(100), 
+	"PW" VARCHAR2(100), 
+	"NAME" VARCHAR2(100), 
+	"TEL" VARCHAR2(100)
+   )
+	
+	
+   
+      CREATE TABLE "HR"."member3" 
+   (	"ID" VARCHAR2(100), 
+	"PW" VARCHAR2(100), 
+	"NAME" VARCHAR2(100), 
+	"TEL" VARCHAR2(100)
+   )
+	
+   
+   INSERT INTO "member" VALUES ('park','1234','park','011')
+
+   
+   INSERT INTO "member" VALUES ('apple','1111','apple','011')
+
+   
+   INSERT INTO "member" VALUES ('ice','2222','ice','012')
+
+   
+   INSERT INTO "member" VALUES ('melon','3333','melon','013')
+   
+   
+   
+   SELECT * FROM "member"  -- 모든 컬럼
+   
+   
+   SELECT id FROM "member"
+   
+   
+   SELECT id, pw FROM "member" 
+   
+   
+   
---
+++ Script-2.sql
@@ -0,0 +1,33 @@
+
+
+
+CREATE TABLE  hobby  ( 
+	hobbyid varchar2(100),
+	content varchar2(100),
+	location varchar2(100)
+	)
+	
+	
+	CREATE TABLE "HR"."HOBBY2" 
+   (	"HOBBYID" VARCHAR2(100), 
+	"CONTENT" VARCHAR2(100), 
+	"LOCATION" VARCHAR2(100)
+   )
+   
+   
+   
+   INSERT INTO "HOBBY" VALUES (100,'run','location') 
+   
+   
+   INSERT INTO "HOBBY" VALUES (200,'book','busan') 
+   
+   
+   SELECT * FROM "HOBBY" 
+
+   SELECT CONTENT  FROM "HOBBY" 
+
+   SELECT LOCATION , CONTENT  FROM "HOBBY" 
+
+
+   
+   
